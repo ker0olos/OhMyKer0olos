96c6eb8074eb4bcbd96244c46e251de83533cc08
Kerolos Zaki <kerolos4zaki@gmail.com>
2018-05-14 00:42:33 +0200

    init

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
-# vscode-theme-generator-quick-start
-
-**Clone and run for a quick way to get started with [vscode-theme-generator](https://github.com/Tyriar/vscode-theme-generator).**
-
-## How to build the theme
-
-You need [NodeJS](https://nodejs.org/en/) installed to run the generator.
-
-```js
-npm install
-```
-
-Make your changes to the colors in index.ts and hit F5 to build the theme and launch the a new VS Code window with your theme available in the command palette (`ctrl`/`cmd+shift+p` > "Color Theme").
-
-## Publishing
-
-When you're ready to publish your theme just fill out the `"name"`, `"displayName", `"publisher"` and `"description"` values in the package.json and [publish it like any other extension](https://code.visualstudio.com/docs/extensions/publish-extension).
+<h1>Purple Oblivion</h1>
+<h4>C#</h4>
+<a href=""><img src="https://gitlab.com/herpproject/purple-oblivione-theme/raw/master/images/csharp.jpg"></a>
+<h4>JS</h4>
+<a href=""><img src="https://gitlab.com/herpproject/purple-oblivione-theme/raw/master/images/js.jpg"></a>
+<h4>XML</h4>
+<a href=""><img src="https://gitlab.com/herpproject/purple-oblivione-theme/raw/master/images/xml.jpg"></a>
+<h4>JSON</h4>
+<a href=""><img src="https://gitlab.com/herpproject/purple-oblivione-theme/raw/master/images/json.jpg"></a>
--- index.ts
+++ index.ts
@@ -1,24 +1,54 @@
 import * as path from 'path';
 import { generateTheme, IColorSet } from 'vscode-theme-generator';
 
-const themeName = 'Generated';
-const colors = {
-  black: '#111111',
+const themeName = 'Purple Oblivion';
+
+const color =
+{
+  dark: '#252527',
+  whitish: '#D3D7CF',
   white: '#FFFFFF',
-  blue: '#5555FF',
+  greenish: '#4EC9B0',
+  green: '#8ED15D',
+  orange: '#ECC116',
+  grey: '#888A85',
   red: '#FF5555',
-  green: '#55FF55',
-  yellow: '#FFFF33'
+  purple: '#AD7EC9',
+  bluish: '#7496B9'
 }
 
-const colorSet: IColorSet = {
-  base: {
-    background: colors.black,
-    foreground: colors.white,
-    color1: colors.blue,
-    color2: colors.red,
-    color3: colors.green,
-    color4: colors.yellow
+const colorSet: IColorSet =
+{
+  base:
+  {
+    background: color.dark,
+    foreground: color.whitish,
+
+    color1: color.whitish,
+    color2: color.orange,
+    color3: color.green,
+    color4: color.whitish
+  },
+  syntax:
+  {
+    comment: color.grey,
+    boolean: color.purple,
+    function: color.red,
+    functionCall: color.white,
+    identifier: color.whitish,
+    keyword: color.purple,
+    number: color.orange,
+    storage: color.greenish,
+    string: color.orange,
+    // comment: color.red,
+    // class: color.red,
+    // classMember: color.red,
+    // type: color.red,
+    // modifier: color.red,
+    cssClass: color.bluish,
+    cssId: color.bluish,
+    cssTag: color.bluish,
+    // markdownQuote: color.red
   }
 };
 
